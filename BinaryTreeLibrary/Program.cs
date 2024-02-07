eca6d51176bd163cc70f41bb7274d090d13a4443
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-02-07 17:40:12 +0200

    .

---
+++ BinaryTreeLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var binaryTree = new BinaryTree<int>();
+
+        binaryTree.Add(8);
+        binaryTree.Add(3);
+        binaryTree.Add(10);
+        binaryTree.Add(1);
+        binaryTree.Add(6);
+        binaryTree.Add(4);
+        binaryTree.Add(7);
+        binaryTree.Add(14);
+        binaryTree.Add(16);
+    }
+}
